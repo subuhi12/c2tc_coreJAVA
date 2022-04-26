0626b1d2f94849c9a777df46faa26a1c98b53240
Aamir.sohail <Aamir.sohail@LAPTOP-4KVCAKM4>
2022-04-26 05:47:02 +0530

    team

---
+++ firstprogram/src/demo.java
@@ -0,0 +1,12 @@
+
+public class demo {
+
+	public static void main(String[] args) {
+	
+		System.out.println("hello beautiful world");
+		
+		String i = "how you doing";
+		System.out.println(i);
+	}
+
+}
