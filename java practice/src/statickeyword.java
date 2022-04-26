162fd812a196e889d50bb0e19393bce0bc5a1007
Aamir.sohail <Aamir.sohail@LAPTOP-4KVCAKM4>
2022-04-26 07:04:52 +0530

    team

--- java practice/src/statickeyword.java
+++ java practice/src/statickeyword.java
@@ -1,19 +1,15 @@
-
-class Demo 
-{
+class Demo {
 
 static
 
 {
 
-System.out.println("static block is invoked");
+System.out.println("static block is invoked"); 
 }
 
 public static void main(String args[])
-{
-	
-
-System.out.println("Hello main");
-
+{ 
+	System.out.println("Hello main");
 }
+
 }
